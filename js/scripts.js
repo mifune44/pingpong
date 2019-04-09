3f5f6e4ca4153953cd513da8a08b3521f6dfd897
Joshua Landry <46336653+mifune44@users.noreply.github.com>
2019-04-08 19:39:50 -0700

    Fleshed out css and jquery. Tuned html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
+// Back end 
+var pingPong = function(count) {
+  var result = [];
+  for(var i = 1; i <= count; i++) {
+    if (i % 15 === 0) {
+      result.push("pingpong");
+    } else if (i % 3 === 0) {
+      result.push("ping");
+    } else if (i % 5 === 0) {
+      result.push("pong");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+};
 
+// Front end 
+$(function() {
+  $("#pingPongForm").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("input#userInput").val());
+    var input = pingPong(userInput);
+    
+    $("#result").empty();
+    input.forEach(function(num) {
+      $("#result").append("<li>" + num + "</li>");
+    });
+    $("#result").fadeIn("slow");
+  });
+});
